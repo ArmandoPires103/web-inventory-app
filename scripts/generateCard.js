23e8029438fa2771e84b5e6db77b3079eb8cae7c
armandopires <armandopires@pursuit.org>
2023-11-05 01:08:18 -0400

    Made generateCard

---
+++ scripts/generateCard.js
@@ -0,0 +1,47 @@
+// Function to create a new card element
+function createCard(name, sport, year, image, price, inStock) {
+    // Create a new card element
+    const li = document.createElement("li");
+    li.classList.add("singleItem");
+
+    // Create an image element and set its attributes
+    const img = document.createElement("img");
+    img.src = image;
+    img.alt = `${name} Card`;
+    li.appendChild(img);
+
+    // Append other card details
+    li.innerHTML += `
+        <br>
+        <strong>Card Name:</strong> ${name}
+        <br>
+        <strong>Sport:</strong> ${sport}
+        <br>
+        <strong>Year:</strong> ${year}
+        <br>
+        <strong>Condition:</strong> (Add your condition logic here)
+        <br>
+        <strong>Price:</strong> $${price}
+        <br>
+        <button class="remove">Remove Item</button>
+    `;
+
+    // Append the new card to the card list
+    const cardList = document.querySelector(".all-cards ul");
+    cardList.appendChild(li);
+}
+
+// Function to generate a new card using the createCard function
+function generateCard(name, sport, year, image, price, inStock) {
+    console.log("Values from form:", name, sport, year, image, price, inStock);
+    createCard(name, sport, year, image, price, inStock);
+}
+
+// Additional code to handle removing items if needed
+const allItems = document.querySelectorAll(".remove");
+
+allItems.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.target.closest('.singleItem').remove();
+    });
+});
---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+const newCardForm = document.getElementById("new-card");
+newCardForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get values from the form
+    const name = document.getElementById("name").value;
+    const sport = document.getElementById("sport").value;
+    const year = document.getElementById("year").value;
+    const image = document.getElementById("image").value;
+    const price = document.getElementById("price").value;
+    const inStock = document.getElementById("inStock").value;
+
+    // Call the function to generate a new card
+    generateCard(name, sport, year, image, price, inStock);
+
+    form.reset();
+});
+
